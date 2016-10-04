293582731678e285f7df6657e94003db2d226adb
Mehraz Ali <mali@C02RT090G8WL.attlocal.net>
2016-10-04 18:14:28 -0400

    Change inline template to point to separate file

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,7 @@ import { ToDo } from './todo/todo';
 
 @Component({
     selector: 'yatd-app',
-    template: `
-    <h1>{{title}}</h1>
-    <div id="todos">
-        <label><input type="checkbox"></label> {{toDo.text}}
-    </div>
-    `
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
     title = 'Yet Another To Do App';
